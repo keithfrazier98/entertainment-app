db0fbd6182752c1d19b705decce7112e2349a1da
Keith Frazier <keithers98@gmail.com>
2022-09-14 19:44:16 -0700

    adjust play btn on mobile

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -11,7 +11,7 @@ export default function PlayButton({ className }: { className?: string }) {
       )}
     >
       <div className="bg-white bg-opacity-25 rounded-full pl-3 py-3 flex text-white items-center">
-        <PlayIcon className="w-6 h-6 scale-150 fill-white" />
+        <PlayIcon className="lg:w-6 lg:h-6 w-8 h-8 lg:scale-150 fill-white" />
         <p className="mx-8 text-lg">Play</p>
       </div>
     </div>
